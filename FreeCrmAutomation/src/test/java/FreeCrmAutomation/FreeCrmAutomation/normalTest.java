8ed2547ab203bbb66aa4f405b7de49f2d4ab3410
arpan <arpan@LAPTOP-6F5CJ392>
2021-05-03 21:17:12 +0530

    First code push

---
+++ FreeCrmAutomation/src/test/java/FreeCrmAutomation/FreeCrmAutomation/normalTest.java
@@ -0,0 +1,30 @@
+package FreeCrmAutomation.FreeCrmAutomation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+
+public class normalTest {
+
+	 static WebDriver driver;
+	 
+	 @Test
+	 public void Setup() {
+		 System.setProperty("webdriver.chrome.driver", "/Users/arpan/Documents/chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+			driver.navigate().to("http://www.google.com");
+			//driver.findElement(By.xpath("q"));
+	 }
+	 
+	@AfterMethod
+	 public void TearDown() {
+		 driver.quit();
+	 }
+}
